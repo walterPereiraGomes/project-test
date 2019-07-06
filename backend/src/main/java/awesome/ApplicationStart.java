773533f2755e42c93adc2e841de3e3ea30714711
Walter Pereira <walter.gt4w@gmail.com>
2019-07-06 11:19:18 -0300

    configurando a base do backend, iniciando spring

---
+++ backend/src/main/java/awesome/ApplicationStart.java
@@ -0,0 +1,13 @@
+package awesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class ApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class, args);
+    }
+}
---
+++ backend/src/main/java/awesome/model/Student.java
@@ -0,0 +1,21 @@
+package awesome.model;
+
+public class Student {
+    private String name;
+
+    public Student (String name) {
+        this.name = name;
+    }
+
+    public Student () {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
