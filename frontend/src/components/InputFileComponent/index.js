37113a7cd9c602ca809ce0f46a4c21ae19bc861d
Walter Pereira <walter.gt4w@gmail.com>
2019-07-15 19:41:22 -0300

    WIP - construindo componente de input

--- frontend/src/components/InputFileComponent/index.js
+++ frontend/src/components/InputFileComponent/index.js
@@ -4,8 +4,16 @@ class InputFile extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.montarObjeto = this.montarObjeto.bind(this);
     this.state = {
       texto: null,
+      card: {
+        tipoRegistro: '',
+        data: '',
+        numeroEstabelecimento: '',
+        tipoCard: '',
+        sequencial: '',
+      },
     };
   }
 
@@ -22,56 +30,33 @@ class InputFile extends React.Component {
     const uflaCard = 'UflaCard';
     const fagammonCard = 'FagammonCard';
 
-    const novoCard = {
-      constante: {},
-      dataGeracao: {},
-      numeroEstabelecimento: {},
-      tipo: {},
-      sequencialArquivo: {},
-    };
+    const text = this.state.texto;
+
+    console.log(text);
 
     if (this.state.texto.indexOf(fagammonCard) !== -1) {
-      const id = this.state.texto.substring(0, 1);
+      console.log('entrei');
+      // this.setState({
+      //   card: {
+      //     tipoRegistro: 'text.substring(0, 1)',
+      //     data: 'text.substring(1, 9)',
+      //     numeroEstabelecimento: 'text.substring(9, 17)',
+      //     tipoCard: 'text.substring(17, 29)',
+      //     sequencial: 'text.substring(29, 36)',
+      //   },
+      // });
 
-      novoCard.constante.inicio = '001';
-      novoCard.constante.fim = '001';
-      novoCard.constante.tam = id.length;
-      novoCard.constante.tipo = typeof (id);
-      novoCard.constante.descricao = 'Tipo de Registro';
-      novoCard.constante.edicaoDados = id;
+      this.setState({
+        card: {
+          tipoRegistro: 'oi',
+          data: 'oi',
+          numeroEstabelecimento: 'toi',
+          tipoCard: 'oi',
+          sequencial: 'oi',
+        },
+      });
 
-      const data = this.state.texto.substring(1, 9);
-      novoCard.dataGeracao.inicio = '002';
-      novoCard.dataGeracao.fim = '009';
-      novoCard.dataGeracao.tam = data.length;
-      novoCard.dataGeracao.tipo = typeof (data);
-      novoCard.dataGeracao.descricao = 'Data de Processamento';
-      novoCard.dataGeracao.edicaoDados = data;
-
-      const numeroEstabelecimento = this.state.texto.substring(10, 17);
-      novoCard.numeroEstabelecimento.inicio = '010';
-      novoCard.numeroEstabelecimento.fim = '017';
-      novoCard.numeroEstabelecimento.tam = numeroEstabelecimento.length;
-      novoCard.numeroEstabelecimento.tipo = typeof (numeroEstabelecimento);
-      novoCard.numeroEstabelecimento.descricao = 'Estabelecimento';
-      novoCard.numeroEstabelecimento.edicaoDados = numeroEstabelecimento;
-
-      const fagammon = this.state.texto.substring(18, 29);
-      novoCard.tipo.inicio = '018';
-      novoCard.tipo.fim = '029';
-      novoCard.tipo.tam = fagammon.length;
-      novoCard.tipo.tipo = typeof (fagammon);
-      novoCard.tipo.descricao = 'Tipo';
-      novoCard.tipo.edicaoDados = fagammon;
-
-      const sequencial = this.state.texto.substring(30, 36);
-      novoCard.sequencialArquivo.inicio = '030';
-      novoCard.sequencialArquivo.fim = '036';
-      novoCard.sequencialArquivo.tam = sequencial.length;
-      novoCard.sequencialArquivo.tipo = typeof (sequencial);
-      novoCard.sequencialArquivo.descricao = 'Sequencia';
-      novoCard.sequencialArquivo.edicaoDados = sequencial;
-      console.log(novoCard);
+      console.log(this.state.card);
     } else if (this.state.texto.indexOf(uflaCard) !== -1) {
       console.log('caiu na ufla');
     }
@@ -86,7 +71,7 @@ class InputFile extends React.Component {
         <br />
         <span>{arquivo}</span>
         <br />
-        <button type="button" onClick={() => this.montarObjeto()}>Enviar</button>
+        <button type="button" onClick={this.montarObjeto}>Enviar</button>
       </div>
     );
   }
